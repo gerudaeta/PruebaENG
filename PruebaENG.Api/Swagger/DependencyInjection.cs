84b3da0bcf6c6b64ae9d98fa2a012b422afcc4d3
gerudaeta <gerudaeta@gmail.com>
2021-12-10 00:52:34 -0300

    Swagger

---
+++ PruebaENG.Api/Swagger/DependencyInjection.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.Extensions.DependencyInjection;
+using Microsoft.OpenApi.Models;
+
+namespace PruebaENG.Api.Swagger;
+
+public static class DependencyInjection
+{
+    public static IServiceCollection AddSwagger(this IServiceCollection services)
+    {
+        services.AddSwaggerGen(swagger =>
+        {
+            swagger.SwaggerDoc("v1", new OpenApiInfo
+            {
+                Version = "v1",
+                Title = "PruebaENG.Api",
+                Description = "Web API de Prueba"
+            });
+            swagger.CustomOperationIds(e => $"{e.ActionDescriptor.RouteValues["action"]}");
+        });
+
+        return services;
+    }
+}
