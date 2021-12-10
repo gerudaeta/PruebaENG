01b9d21c7406c11f865888fcf4e5afa7f97afc32
gerudaeta <gerudaeta@gmail.com>
2021-12-10 00:54:00 -0300

    Modelo User

---
+++ PruebaENG.Domain/Entities/User.cs
@@ -0,0 +1,12 @@
+﻿using PruebaENG.Domain.Common;
+
+namespace PruebaENG.Domain.Entities;
+
+public class User : AuditableEntity, IHasDomainEvent
+{
+    public int Id { get; set; }
+    public string Name { get; set; }
+    public DateTime BirthDate { get; set; }
+    
+    public List<DomainEvent> DomainEvents { get; set; }
+}
