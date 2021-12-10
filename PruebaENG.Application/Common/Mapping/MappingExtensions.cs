c90dc6db83b102261a5a824e8dcad531e66a0c23
gerudaeta <gerudaeta@gmail.com>
2021-12-10 00:54:53 -0300

    automapper

---
+++ PruebaENG.Application/Common/Mapping/MappingExtensions.cs
@@ -0,0 +1,18 @@
+﻿using AutoMapper;
+using AutoMapper.QueryableExtensions;
+using Microsoft.EntityFrameworkCore;
+using PruebaENG.Application.Common.Models;
+
+namespace PruebaENG.Application.Common.Mapping;
+
+public static class MappingExtensions
+{
+    public static Task<PaginatedList<TDestination>> PaginatedListAsync<TDestination>(this IQueryable<TDestination> queryable, int pageNumber, int pageSize)
+        => PaginatedList<TDestination>.CreateAsync(queryable, pageNumber, pageSize);
+
+    public static Task<List<TDestination>> ProjectToListAsync<TDestination>(this IQueryable queryable, IConfigurationProvider configuration)
+        => queryable.ProjectTo<TDestination>(configuration).ToListAsync();
+
+    public static PaginatedList<TDestination> PaginatedListSync<TDestination>(this IQueryable<TDestination> queryable, int pageNumber, int pageSize)
+        => PaginatedList<TDestination>.Create(queryable, pageNumber, pageSize);
+}
