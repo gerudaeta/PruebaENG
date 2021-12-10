6097041f115719f9d3e749d0911bd7fc6264443b
gerudaeta <gerudaeta@gmail.com>
2021-12-10 00:55:48 -0300

    interfaz DbContext

---
+++ PruebaENG.Application/Common/Interfaces/IApplicationDbContext.cs
@@ -0,0 +1,9 @@
+﻿using Microsoft.EntityFrameworkCore;
+using PruebaENG.Domain.Entities;
+
+namespace PruebaENG.Application.Common.Interfaces;
+
+public interface IApplicationDbContext
+{
+    DbSet<User> Users { get; set; }
+}
