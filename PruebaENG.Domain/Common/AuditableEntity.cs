2edfcbcab617db19adc4e78c0d91ae0a5159383b
gerudaeta <gerudaeta@gmail.com>
2021-12-10 00:54:14 -0300

    Clases common

---
+++ PruebaENG.Domain/Common/AuditableEntity.cs
@@ -0,0 +1,7 @@
+﻿namespace PruebaENG.Domain.Common;
+
+public abstract class AuditableEntity
+{
+    public DateTime CreatedDateTime { get; set; }
+    public DateTime? UpdatedDateTime { get; set; }
+}
