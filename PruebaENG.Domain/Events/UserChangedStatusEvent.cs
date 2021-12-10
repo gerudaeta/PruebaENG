1203361281440e6ec88aefb0e110fa61f1f4e086
gerudaeta <gerudaeta@gmail.com>
2021-12-10 00:53:54 -0300

    Eventos

---
+++ PruebaENG.Domain/Events/UserChangedStatusEvent.cs
@@ -0,0 +1,13 @@
+﻿using PruebaENG.Domain.Entities;
+
+namespace PruebaENG.Domain.Events;
+
+public class UserChangedStatusEvent
+{
+    public UserChangedStatusEvent(User user)
+    {
+        User = user;
+    }
+
+    public User User { get; }
+}
---
+++ PruebaENG.Domain/Events/UserCreatedEvent.cs
@@ -0,0 +1,14 @@
+﻿using PruebaENG.Domain.Common;
+using PruebaENG.Domain.Entities;
+
+namespace PruebaENG.Domain.Events;
+
+public class UsuarioCreadoEvent : DomainEvent
+{
+    public UsuarioCreadoEvent(User user)
+    {
+        User = user;
+    }
+
+    public User User { get; }
+}
