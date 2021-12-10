4cf5456bbe82cb95511f9beb4d63c59e578ed817
gerudaeta <gerudaeta@gmail.com>
2021-12-10 00:53:05 -0300

    DateTimeService

---
+++ PruebaENG.Infrastructure/Services/DateTimeService.cs
@@ -0,0 +1,8 @@
+﻿using PruebaENG.Application.Common.Interfaces;
+
+namespace PruebaENG.Infrastructure.Services;
+
+public class DateTimeService : IDateTime
+{
+    public DateTime Now => DateTime.Now;
+}
