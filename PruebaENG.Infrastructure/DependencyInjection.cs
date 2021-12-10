bde3d928419c19ef6ff6f7ace2189ce018bcb917
gerudaeta <gerudaeta@gmail.com>
2021-12-10 00:53:47 -0300

    Inyeccion de dependencia

---
+++ PruebaENG.Infrastructure/DependencyInjection.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.AspNetCore.Identity;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using PruebaENG.Application.Common.Interfaces;
+using PruebaENG.Infrastructure.Persistence;
+using PruebaENG.Infrastructure.Services;
+
+namespace PruebaENG.Infrastructure;
+
+public static class DependencyInjection
+{
+    public static IServiceCollection AddInfrastructure(this IServiceCollection services, IConfiguration configuration)
+    {
+        services.AddDbContext<ApplicationDbContext>(options =>
+            options.UseSqlServer(
+                configuration.GetConnectionString("EFCoreDatabase"),
+                b => b.MigrationsAssembly(typeof(ApplicationDbContext).Assembly.FullName)));
+
+        services.AddScoped<IApplicationDbContext>(provider => provider.GetService<ApplicationDbContext>());
+
+        services.AddScoped<IDomainEventService, DomainEventService>();
+
+        services.AddTransient<IDateTime, DateTimeService>();
+
+        return services;
+    }
+}
