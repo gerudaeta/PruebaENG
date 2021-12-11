a077afff115b22be991805d863e5d736e675e615
gerudaeta <gerudaeta@gmail.com>
2021-12-11 01:01:51 -0300

    se agrega campo estado a la entidad usuario

--- PruebaENG.Domain/Entities/User.cs
+++ PruebaENG.Domain/Entities/User.cs
@@ -7,6 +7,6 @@ public class User : AuditableEntity, IHasDomainEvent
     public int Id { get; set; }
     public string Name { get; set; }
     public DateTime BirthDate { get; set; }
-    
+    public bool Status { get; set; } = true;
     public List<DomainEvent> DomainEvents { get; set; }
 }
