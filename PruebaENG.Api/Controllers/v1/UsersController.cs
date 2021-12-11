6b3173e6004dc04007f199145e967d7e6a096ba6
gerudaeta <gerudaeta@gmail.com>
2021-12-11 01:00:58 -0300

    endpoint -> create y change status user

--- PruebaENG.Api/Controllers/v1/UsersController.cs
+++ PruebaENG.Api/Controllers/v1/UsersController.cs
@@ -1,6 +1,34 @@
-﻿namespace PruebaENG.Api.Controllers.v1;
+﻿using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using PruebaENG.Application.Common.Wrapper;
+using PruebaENG.Application.Users.Commands.ChangeStatusUser;
+using PruebaENG.Application.Users.Commands.CreateUser;
+
+namespace PruebaENG.Api.Controllers.v1;
 
 public class UsersController : ApiControllerBase
 {
+    [HttpPost("CreateUser")]
+    [ProducesResponseType(StatusCodes.Status200OK, Type = typeof(Response<int>))]
+    public async Task<ActionResult<Response<int>>> CreateAgenda(CreateUserRequest request)
+    {
+        return Ok(await Mediator.Send(request));
+    }
     
+    [HttpPut("ChangeStatusUser/{id:int}")]
+    [ProducesResponseType(StatusCodes.Status400BadRequest)]
+    [ProducesResponseType(StatusCodes.Status204NoContent)]
+    [ProducesResponseType(StatusCodes.Status200OK, Type = typeof(Response<int>))]
+    public async Task<ActionResult> ChangeStatusUser(int id, ChangeStatusUserRequest request)
+    {
+        if (id != request.Id)
+        {
+            return BadRequest();
+        }
+
+        await Mediator.Send(request);
+
+        return NoContent();
+    }
 }
