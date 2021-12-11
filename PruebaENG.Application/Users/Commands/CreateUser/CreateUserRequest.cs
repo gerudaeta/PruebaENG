48130757b54f0e1c3dd3407552a047ccc02671b8
gerudaeta <gerudaeta@gmail.com>
2021-12-11 01:02:15 -0300

    funcionalidad crear user

---
+++ PruebaENG.Application/Users/Commands/CreateUser/CreateUserRequest.cs
@@ -0,0 +1,48 @@
+﻿using Microsoft.Extensions.Logging;
+using PruebaENG.Application.Common.Interfaces;
+using PruebaENG.Application.Common.Wrapper;
+using PruebaENG.Domain.Constants;
+using PruebaENG.Domain.Entities;
+
+namespace PruebaENG.Application.Users.Commands.CreateUser;
+
+public class CreateUserRequest : IRequestWrapper<int>
+{
+    public string Name { get; set; }
+    public DateTime BirthDate { get; set; }
+}
+
+public class CreateUserRequestHandler : IRequestHandlerWrapper<CreateUserRequest, int>
+{
+    private readonly IApplicationDbContext _applicationDbContext;
+    private readonly ILogger<CreateUserRequestHandler> _logger;
+
+    public CreateUserRequestHandler(IApplicationDbContext applicationDbContext, ILogger<CreateUserRequestHandler> logger)
+    {
+        _applicationDbContext = applicationDbContext;
+        _logger = logger;
+    }
+
+    public async Task<Response<int>> Handle(CreateUserRequest request, CancellationToken cancellationToken)
+    {
+        var user = new User
+        {
+            Name = request.Name,
+            BirthDate = request.BirthDate
+        };
+
+        await _applicationDbContext.Users.AddAsync(user, cancellationToken);
+
+        try
+        {
+            await _applicationDbContext.SaveChangesAsync(cancellationToken);
+            return await Response<int>.SuccessAsync(user.Id, Message.RegistrationSuccessful);
+        }
+        catch (Exception e)
+        {
+            _logger.LogCritical($"Error saving user: {e.Message} - {e.StackTrace}");
+            return await Response<int>.FailAsync(Message.RegistrationError);
+        }
+        
+    }
+}
---
+++ PruebaENG.Application/Users/Commands/CreateUser/CreateUserRequestValidator.cs
@@ -0,0 +1,17 @@
+﻿using FluentValidation;
+
+namespace PruebaENG.Application.Users.Commands.CreateUser;
+
+public class CreateUserRequestValidator : AbstractValidator<CreateUserRequest>
+{
+    public CreateUserRequestValidator()
+    {
+        RuleFor(x => x.Name)
+            .NotEmpty()
+            .WithMessage("The name field is required.");
+        
+        RuleFor(x => x.BirthDate)
+            .NotEmpty()
+            .WithMessage("The BirthDate field is required.");
+    }
+}
