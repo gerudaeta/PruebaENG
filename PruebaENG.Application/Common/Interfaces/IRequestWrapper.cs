12e029cde455eb2d7921d32569a77b7d245e8e87
gerudaeta <gerudaeta@gmail.com>
2021-12-11 01:01:18 -0300

    interfaz IRequestWrapper para handler

---
+++ PruebaENG.Application/Common/Interfaces/IRequestWrapper.cs
@@ -0,0 +1,14 @@
+﻿using MediatR;
+using PruebaENG.Application.Common.Wrapper;
+
+namespace PruebaENG.Application.Common.Interfaces;
+
+public interface IRequestWrapper<T> : IRequest<Response<T>>
+{
+
+}
+
+public interface IRequestHandlerWrapper<TIn, TOut> : IRequestHandler<TIn, Response<TOut>> where TIn : IRequestWrapper<TOut>
+{
+
+}
