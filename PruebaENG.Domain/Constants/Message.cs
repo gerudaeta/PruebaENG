0ffa8f3f6bb7eeddf91c007f18de1bc276c13d62
gerudaeta <gerudaeta@gmail.com>
2021-12-11 01:02:02 -0300

    mensajes

---
+++ PruebaENG.Domain/Constants/Message.cs
@@ -0,0 +1,10 @@
+﻿namespace PruebaENG.Domain.Constants;
+
+public static class Message
+{
+    public const string RegistrationSuccessful = "Successful Registration";
+    public const string ModificationSuccessful = "Successful Modification";
+    public const string ChangeStatusSuccessful = "Successful Status Change";
+    public const string RegistrationError = "Error Registration";
+    public const string NotRegister = "This record is not registered.";
+}
