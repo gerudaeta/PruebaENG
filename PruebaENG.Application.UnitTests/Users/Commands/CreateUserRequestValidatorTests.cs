1b6d9e681b70b51f5b9a639fcaf087e74d577208
gerudaeta <gerudaeta@gmail.com>
2021-12-13 11:13:47 -0300

    unit test validator

---
+++ PruebaENG.Application.UnitTests/Users/Commands/CreateUserRequestValidatorTests.cs
@@ -0,0 +1,28 @@
+﻿using FluentValidation.TestHelper;
+using PruebaENG.Application.Users.Commands.CreateUser;
+using PruebaENG.Domain.Entities;
+using Xunit;
+
+namespace PruebaENG.Application.UnitTests.Users.Commands;
+
+public class CreateUserRequestValidatorTests
+{
+    private readonly CreateUserRequestValidator _validator;
+    
+    public CreateUserRequestValidatorTests()
+    {
+        _validator = new CreateUserRequestValidator();
+    }
+
+    [Fact]
+    public void Validator_UserFieldNull_ReturnValidationError()
+    {
+        var request = new CreateUserRequest();
+
+        var result = _validator.TestValidate(request);
+        
+        result.ShouldHaveValidationErrorFor(x => x.Name);
+        result.ShouldHaveValidationErrorFor(x => x.BirthDate);
+    }
+    
+}
